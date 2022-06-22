9e57bb66f1389d6e318a81cb503e07b6d75d711f
kiridaruma <www2480www@gmail.com>
2022-06-23 00:06:48 +0900

    フィールドをprivateにしてgetterを実装

--- src/config.rs
+++ src/config.rs
@@ -1,18 +1,18 @@
 use serde::Deserialize;
 use std::io;
 use std::io::Read;
-use std::{fs::File, path::PathBuf};
+use std::{fs::File, path::Path, path::PathBuf};
 use toml;
 
 #[derive(Deserialize)]
 pub struct Config {
-    pub data_dir: PathBuf,
-    pub bcdice_url: String,
-    pub room_count_limit: u32,
-    pub room_name_limit: u32,
-    pub room_delete_interval_sec: u32,
-    pub message_length_limit: u32,
-    pub nickname_length_limit: u32,
+    data_dir: PathBuf,
+    bcdice_url: String,
+    room_count_limit: u32,
+    room_name_limit: u32,
+    room_delete_interval_sec: u32,
+    message_length_limit: u32,
+    nickname_length_limit: u32,
 }
 
 pub enum Error {
@@ -21,13 +21,41 @@ pub enum Error {
 }
 
 impl Config {
-    pub fn load(path: &PathBuf) -> Result<Self, Error> {
+    pub fn data_dir(&self) -> &Path {
+        &self.data_dir
+    }
+
+    pub fn bcdice_url(&self) -> &str {
+        &self.bcdice_url
+    }
+
+    pub fn room_count_limit(&self) -> &u32 {
+        &self.room_count_limit
+    }
+
+    pub fn room_name_limit(&self) -> &u32 {
+        &self.room_name_limit
+    }
+
+    pub fn room_delete_interval_sec(&self) -> &u32 {
+        &self.room_delete_interval_sec
+    }
+
+    pub fn message_length_limit(&self) -> &u32 {
+        &self.message_length_limit
+    }
+
+    pub fn nickname_length_limit(&self) -> &u32 {
+        &self.nickname_length_limit
+    }
+
+    pub fn load(path: &Path) -> Result<Self, Error> {
         let file_text = Self::load_file(path).map_err(|e| Error::LoadFailed(e))?;
 
         toml::from_str(file_text.as_str()).map_err(|e| Error::ParseFailed(e))
     }
 
-    fn load_file(path: &PathBuf) -> Result<String, io::Error> {
+    fn load_file(path: &Path) -> Result<String, io::Error> {
         let mut file = File::open(path)?;
         let mut file_text = String::new();
         file.read_to_string(&mut file_text)?;
@@ -40,7 +68,7 @@ impl Config {
 mod test {
     use crate::config::{Config, Error};
     use std::fs::remove_file;
-    use std::path::PathBuf;
+    use std::path::Path;
     use std::{fs::File, io::Write};
 
     #[test]
@@ -69,7 +97,7 @@ nickname_length_limit=20
         "#,
         );
 
-        let file_path = PathBuf::from("./config_valid_toml_test.toml");
+        let file_path = Path::new("./config_valid_toml_test.toml");
         let mut file = File::create(&file_path).unwrap();
         file.write_all(valid_toml_text.as_bytes()).unwrap();
         let result = Config::load(&file_path);
@@ -80,7 +108,7 @@ nickname_length_limit=20
 
     #[test]
     fn failed_file_not_exists() {
-        let not_exists_file_path = PathBuf::from("./not_exists");
+        let not_exists_file_path = Path::new("./not_exists");
         let result = Config::load(&not_exists_file_path);
         assert_eq!(true, result.is_err());
         match result {
@@ -92,7 +120,7 @@ nickname_length_limit=20
     #[test]
     fn failed_parse_toml() {
         let invalid_toml_text = String::from("The niece of time");
-        let file_path = PathBuf::from("./config_invalid_toml_test.toml");
+        let file_path = Path::new("./config_invalid_toml_test.toml");
         let mut file = File::create(&file_path).unwrap();
         file.write_all(invalid_toml_text.as_bytes()).unwrap();
         let result = Config::load(&file_path);
